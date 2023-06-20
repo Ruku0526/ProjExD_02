8853ae9a0d0732b770b5e3b6471b46512c2100cf
Ruku0526 <c0a22076ac@edu.teu.ac.jp>
2023-06-20 17:10:06 +0900

    追加機能２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -12,6 +12,8 @@ delta ={
     pg.K_RIGHT: (+5,0),
 }
 
+bd_imgs = []
+
 def check_bound(rect: pg.Rect) -> tuple[bool, bool]:
     """
     こうかとんRectや爆弾Rectが画面外 or 画面内かを判定する関数
@@ -24,6 +26,32 @@ def check_bound(rect: pg.Rect) -> tuple[bool, bool]:
     if rect.top < 0 or HEIGHT < rect.bottom: #縦方向判定
         tate = False
     return yoko, tate
+def  roto(sum):
+    kk_img_o = pg.image.load("ex02/fig/3.png")
+    kk_img = pg.transform.flip(kk_img_o,True,False)
+    img1 = pg.transform.rotozoom(kk_img_o, 0, 2.0) 
+    img2 = pg.transform.rotozoom(kk_img_o, 45, 2.0)
+    img3 = pg.transform.rotozoom(kk_img_o, -45, 2.0)
+    img4 = pg.transform.rotozoom(kk_img_o, 90, 2.0)
+    img5 = pg.transform.rotozoom(kk_img, 90, 2.0)
+    img6 = pg.transform.rotozoom(kk_img, 0, 2.0)
+    img7 = pg.transform.rotozoom(kk_img, 45, 2.0)
+    img8 = pg.transform.rotozoom(kk_img, -45, 2.0)
+
+    imgs = {
+        (0,0):img1,
+        (0,5):img4,
+        (0,-5):img5,
+        (5,5):img8,
+        (5,-5):img7,
+        (-5,5):img2,
+        (-5,-5):img3,
+        (-5,0):img1,
+        (5,0):img6
+
+
+    }
+    return imgs[tuple(sum)]
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -33,6 +61,7 @@ def main():
     kk_rct = kk_img.get_rect()
     bd_img = pg.Surface((20,20))
     pg.draw.circle(bd_img,(255,0,0),(10,10),10)
+    bd_imgs.append(bd_img)
     bd_img.set_colorkey((0,0,0))
     x = random.randint(0,WIDTH)
     y = random.randint(0,HEIGHT)
@@ -48,7 +77,7 @@ def main():
                 return
         if kk_rct.colliderect(bd_rct):
             print("game over")
-            return #game over
+            return 
         key_lst=pg.key.get_pressed()
         sum_mv = [0,0]
         for k, mv in delta.items():
@@ -58,11 +87,13 @@ def main():
         kk_rct.move_ip(sum_mv[0], sum_mv[1])
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
-
+        kk_img = roto(sum_mv)
+        accs = [a for a in range(1,11)]
+        avx,avy = vx*accs[min(tmr//500,9)], vy*accs[min(tmr//500,9)]
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rct)
         screen.blit(bd_img, bd_rct)
-        bd_rct.move_ip(vx,vy)
+        bd_rct.move_ip(avx,avy)
         yoko, tate = check_bound(bd_rct)
         if not yoko:
             vx *= -1
